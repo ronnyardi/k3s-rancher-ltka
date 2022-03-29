26f696b839d95253f47308b58c4741e3c414713b
ronnyardi <ronnyardianto29@gmail.com>
2022-03-30 04:14:44 +0700

    Update Main Backend TFC

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 terraform {
-    required_providers {
-        digitalocean = {
-            source = "digitalocean/digitalocean"
-            version = "1.22.2"
-        }
-    }
+  required_providers {
+      digitalocean = {
+          source = "digitalocean/digitalocean"
+          version = "1.22.2"
+      }
+  }
 
   cloud {
     organization = "rancher-mylabs"
