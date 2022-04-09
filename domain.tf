92437914f94c078e62114b2570dabfb5d78e7746
ronnyardi <18118039@telecom.stei.itb.ac.id>
2022-04-09 17:23:31 +0700

    add resources domain

--- domain.tf
+++ domain.tf
@@ -1,4 +1,4 @@
-resource "digitalocean_domain" "default" {
+resource "digitalocean_domain" "k3s_domain" {
   name       = "rancher-mylabs.tech"
   ip_address = digitalocean_loadbalancer.k3s_lb.ip
 }
