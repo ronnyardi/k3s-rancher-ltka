24ca2b751781c42d69e82f5f2805ef52c0e15782
ronnyardi <18118039@telecom.stei.itb.ac.id>
2022-04-10 09:34:38 +0700

    fix lb syntax

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -4,36 +4,34 @@ resource "digitalocean_loadbalancer" "k3s_lb" {
   vpc_uuid = digitalocean_vpc.k3s_vpc.id
 
   forwarding_rule {
+    tls_passthrough = true
     entry_port      = 6443
     entry_protocol  = "https"
 
     target_port     = 6443
     target_protocol = "https"
+  }
 
+  forwarding_rule {
     tls_passthrough = true
+    entry_port      = 443
+    entry_protocol  = "https"
+
+    target_port     = 443
+    target_protocol = "https"
+  }
+
+  forwarding_rule {
+    entry_port      = 80
+    entry_protocol  = "http"
+
+    target_port     = 80
+    target_protocol = "http"
   }
 
   healthcheck {
     port     = 6443
     protocol = "tcp"
-  }
-
-  forwarding_rule {
-    entry_port     = 443
-    entry_protocol = "https"
-
-    target_port     = 443
-    target_protocol = "https"
-
-    certificate_name = digitalocean_certificate.k3s-cert.name
-  }
-
-  forwarding_rule {
-    entry_port     = 80
-    entry_protocol = "http"
-
-    target_port     = 80
-    target_protocol = "http"
   }
 
   droplet_tag = local.server_tag
