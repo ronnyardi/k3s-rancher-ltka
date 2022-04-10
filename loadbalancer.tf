4ed7790c70c1401ffe2a76c1186359afee7b214a
ronnyardi <18118039@telecom.stei.itb.ac.id>
2022-04-11 05:37:22 +0700

    fix whitespace syntax

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -2,33 +2,30 @@ resource "digitalocean_loadbalancer" "k3s_lb" {
   name     = "k3s-loadbalancer"
   region   = var.region
   vpc_uuid = digitalocean_vpc.k3s_vpc.id
-
   forwarding_rule {
-    certificate_name  = digitalocean_certificate.k3s-cert.name
-    entry_port        = 6443
-    entry_protocol    = "https"
-    target_port       = 6443
-    target_protocol   = "https"
+    certificate_name = digitalocean_certificate.k3s-cert.name
+    entry_port       = 6443
+    entry_protocol   = "https"
+    target_port      = 6443
+    target_protocol  = "https"
   }
   forwarding_rule {
-    certificate_name  = digitalocean_certificate.k3s-cert.name
-    entry_port        = 443
-    entry_protocol    = "https"
-    target_port       = 443
-    target_protocol   = "https"
+    certificate_name = digitalocean_certificate.k3s-cert.name
+    entry_port       = 443
+    entry_protocol   = "https"
+    target_port      = 443
+    target_protocol  = "https"
   }
   forwarding_rule {
-    entry_port        = 80
-    entry_protocol    = "http"
-    target_port       = 80
-    target_protocol   = "http"
+    entry_port       = 80
+    entry_protocol   = "http"
+    target_port      = 80
+    target_protocol  = "http"
   }
-
   healthcheck {
     port     = 6443
     protocol = "tcp"
   }
-
   droplet_tag = local.server_tag
 }
 
